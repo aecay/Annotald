6e7939d1b41c0c0a41b043a04fad0ccdf08546fc
Aaron Ecay <aaronecay@gmail.com>
2014-01-06 03:40:14 -0500

    file.dropbox.js: jshint

--- webapp/js/file-dropbox.js
+++ webapp/js/file-dropbox.js
@@ -1,12 +1,12 @@
 /*global require: false, exports: true, Dropbox: false */
 
 var Q = require ("q"),
-    Dropbox_core = require("./dropbox"),
+    DropboxCore = require("./ext/dropbox"),
     notify = require("./ui-log"),
     $ = require("jquery");
 
-var client = new Dropbox_core.Client({ key: "rw6m6r2gi34luhp" });
-client.authDriver(new Dropbox_core.AuthDriver.Popup(
+var client = new DropboxCore.Client({ key: "rw6m6r2gi34luhp" });
+client.authDriver(new DropboxCore.AuthDriver.Popup(
     {receiverUrl:"/html/oauth_receiver.html"}));
 
 exports.writeFile = function writeFileDropbox (path, content) {
@@ -17,7 +17,7 @@ exports.writeFile = function writeFileDropbox (path, content) {
             deferred.reject(error);
             return;
         }
-        client.writeFile(path, content, function (error, stat) {
+        client.writeFile(path, content, function (error /*, stat*/) {
             if (error) {
                 notify.error("Error saving file to Dropbox: " + error);
                 deferred.reject();
@@ -63,7 +63,7 @@ exports.readFile = function readFileDropbox (path) {
             },
             linktype: "direct",
             multiselect: false,
-            extensions: ['.psdx']
+            extensions: [".psdx"]
         });
     }
     return deferred.promise;
