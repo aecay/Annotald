43b0b39c50b407f3de111514990e5139767a39b3
Aaron Ecay <aaronecay@gmail.com>
2014-01-06 03:38:24 -0500

    gruntfile: fix up build, main.html: match

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,10 +14,10 @@ module.exports = function (grunt) {
                     ,'node_modules/brace/theme/xcode.js'
                     ,'node_modules/brace/mode/javascript.js'
                     ,'node_modules/q/q.js'
-                    ,'webapp/js/dropbox.js'
-                    ,'webapp/js/growl.js'
+                    ,'webapp/js/ext/dropbox.js'
+                    ,'webapp/js/ext/growl.js'
                 ],
-                dest: 'webapp/js/build/ext.js',
+                dest: 'webapp/build/ext.js',
                 options: {
                     alias: ['bower_components/jquery/jquery.js:jquery',
                             'node_modules/react/react.js:react',
@@ -27,14 +27,14 @@ module.exports = function (grunt) {
                             'node_modules/brace/theme/xcode.js:brace/theme/xcode',
                             'node_modules/brace/mode/javascript.js:brace/mode/javascript',
                             'node_modules/q/q.js:q',
-                            'webapp/js/dropbox.js:dropbox',
-                            'webapp/js/growl.js:growl'
+                            'webapp/js/ext/dropbox.js:dropbox',
+                            'webapp/js/ext/growl.js:growl'
                            ]
                 }
             },
-            dist: {
+            annotald: {
                 src: ['webapp/js/main.js'],
-                dest: 'webapp/js/build/web.js',
+                dest: 'webapp/build/web.js',
                 options: {
                     debug: true,
                     standalone: "annotald",
@@ -53,33 +53,92 @@ module.exports = function (grunt) {
             }
         },
         external_sourcemap: {
-            build: {
+            annotald: {
                 files: {
-                "webapp/js/build": ["webapp/js/build/ext.js",
-                                    "webapp/js/build/web.js"]
+                    "webapp/build": ["webapp/build/web.js"]
                 }
             }
         },
         jasmine: {
-            src: "webapp/js/build/web-bundle.js",
+            src: "webapp/build/web-bundle.js",
             options: {
                 specs: "test/build/spec-entry.js"
             }
         },
         jshint: {
             files: ["webapp/js/**/*.js",
-                    "!webapp/js/build/**",
                     "!webapp/js/ext/**"],
             options: {
                 jshintrc: "jshintrc"
             }
         },
-        watch: {
-            dist: {
-                files: ['webapp/js/*.js'],
-                tasks: ['browserify:dist'],
+        uglify: {
+            annotald: {
+                src: "webapp/build/web.js",
+                dest: "webapp/build/web.min.js",
                 options: {
-                    livereload: true
+                    sourceMap: "webapp/build/web.min.js.map",
+                    sourceMapIn: "webapp/build/web.js.map",
+                    preserveComments: 'some',
+                    sourceMappingURL: '/web.min.js.map',
+                    sourceMapRoot: '/'
+                }
+            },
+            external: {
+                src: "webapp/build/ext.js",
+                dest: "webapp/build/ext.min.js",
+                options: {
+                    preserveComments: 'some'
+                }
+            }
+        },
+        cssmin: {
+            annotald: {
+                src: ["webapp/css/growl.css", "webapp/css/vex.css",
+                      "webapp/css/vex-theme-default.css",
+                      "webapp/css/main.css"],
+                dest: "webapp/build/min.css"
+            }
+        },
+        watch: {
+            annotald: {
+                files: ['webapp/js/*.js'],
+                tasks: ['build-annotald']
+            },
+            css: {
+                files: ['webapp/css/*.css'],
+                tasks: ['build-css']
+            },
+            html: {
+                files: ['webapp/html/*.html'],
+                tasks: ['build-html']
+            },
+            options: {
+                livereload: true
+            }
+        },
+        copy: {
+            main: {
+                src: "webapp/html/main.html",
+                dest: "webapp/build/main.html"
+            },
+            oauth_receiver: {
+                src: "webapp/html/oauth_receiver.html",
+                dest: "webapp/build/oauth_receiver.html"
+            }
+        },
+        clean: {
+            build: {
+                src: ["webapp/build/ext.js",
+                      "webapp/build/web.js",
+                      "webapp/build/web.js.map"]
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8888,
+                    base: 'webapp/build'
                 }
             }
         }
@@ -90,7 +149,25 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-external-sourcemap');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('build', ['browserify']);
+    grunt.registerTask('build-external', ['browserify:external',
+                                          'uglify:external',
+                                          'clean:build']);
+    grunt.registerTask('build-annotald', ['browserify:annotald',
+                                          'external_sourcemap:annotald',
+                                          'uglify:annotald',
+                                          'clean:build']);
+    grunt.registerTask('build-css', ['cssmin']);
+    grunt.registerTask('build-html', ['copy:main','copy:oauth_receiver']);
+
+    grunt.registerTask('build', ['build-external','build-annotald',
+                                 'build-css','build-html']);
     grunt.registerTask('test', ['build', 'jasmine']);
+
+    grunt.registerTask('default', ['build','connect','watch']);
 };
