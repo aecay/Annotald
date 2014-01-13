720bb1f4e0685a6575e6ed858149c8ca86657ef4
Aaron Ecay <aaronecay@gmail.com>
2014-01-13 18:28:45 -0500

    add entry-points.ts

---
+++ webapp/js/treedrawing/entry-points.ts
@@ -0,0 +1,24 @@
+///<reference path="./../../../types/all.d.ts" />
+
+import s = require("./struc-edit");
+import undo = require("./undo");
+import n = require("./node-edit");
+import selection = require("./selection");
+import search = require("./search");
+
+export var leafAfter = s.leafAfter;
+export var leafBefore = s.leafBefore;
+export var setLabel = s.setLabel;
+export var makeNode = s.makeNode;
+export var coIndex = s.coIndex;
+export var toggleCollapsed = false; // TODO
+export var splitWord = false; // TODO
+export var toggleExtension = s.toggleExtension;
+export var pruneNode = s.pruneNode;
+export var undo = undo.undo;
+export var redo = undo.redo;
+export var editNode = n.editNode;
+export var clearSelection = selection.clearSelection;
+export var toggleLemmata = false; // TODO
+export var displayRename = false; // TODO
+export var search = search.search;
