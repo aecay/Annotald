5715ed6540b823a737d2c73e7bf7fb3231c1d623
Aaron Ecay <aaronecay@gmail.com>
2014-02-19 14:30:18 -0500

    label convert work

--- webapp/js/treedrawing/global.ts
+++ webapp/js/treedrawing/global.ts
@@ -1,2 +1,11 @@
+
+// TODO: is there a way to import only the decls?
+import labelConvert = require("./label-convert");
+
 export var lastEventWasMouse : boolean = false;
 export var lastSavedState : string = "";
+export var labelMapping : labelConvert.LabelMap = {
+    defaults: {},
+    defaultSubcategories: [],
+    byLabel: {}
+};
--- webapp/js/treedrawing/label-convert.ts
+++ webapp/js/treedrawing/label-convert.ts
@@ -2,6 +2,100 @@
 
 import utils = require("./utils");
 import globals = require("./global");
+import _ = require("lodash");
+
+export interface MatchSpec {
+    category?: string;
+    subcategory?: string;
+    // TODO: proper type
+    metadata?: { [key : string ] : any };
+}
+
+export interface LabelMapAction {
+    key : string;
+    // TODO: proper type
+    value : any;
+}
+
+export interface LabelMap {
+    defaults : { [key: string] : LabelMapAction };
+    defaultSubcategories: string[];
+    byLabel : {
+        [label: string] : {
+            subcategories: string[];
+            metadataKeys: { [key: string] : LabelMapAction }
+        }
+    };
+}
+
+function isValidSubcategoryForCategory (subcat : string,
+                                        cat : string,
+                                        mapping : LabelMap)
+: boolean {
+    return (mapping.defaultSubcategories.indexOf(subcat) >= 0 ||
+            mapping.byLabel[cat].subcategories.indexOf(subcat) >= 0);
+}
+
+export function matchMetadataAgainstObject (key : string, value : any,
+                                            object : { [key : string] : any }) {
+    if (!object[key]) {
+        return false;
+    }
+    if (object[key] instanceof String) {
+        return object[key] === value;
+    }
+    return _.all(_.forOwn(object[key],
+                          function (v : any, k : string) : boolean {
+                              if (!value[k]) return false;
+                              return matchMetadataAgainstObject(k, value[k], v);
+                          }));
+}
+
+export function nodeMatchesSpec (node : Element, spec : MatchSpec) : boolean {
+    if (spec.category) {
+        if (node.getAttribute("data-category") !== spec.category) {
+            return false;
+        }
+    }
+    if (spec.subcategory) {
+        if (node.getAttribute("data-subcategory") !== spec.subcategory) {
+            return false;
+        }
+    }
+    if (spec.metadata) {
+        var md = JSON.parse(node.getAttribute("data-metadata"));
+        var res = _.all(_.forOwn(spec.metadata, function (value : any,
+                                                          key : string)
+                                 : boolean {
+                                     return matchMetadataAgainstObject(
+                                         key, value, md);
+                                 }));
+    }
+}
+
+export function labelToMatchSpec (label : string, mapping : LabelMap)
+: MatchSpec {
+    var pieces = label.split("-");
+    var r : MatchSpec = {};
+    r.category = pieces.shift();
+    var submap = mapping.byLabel[r.category].metadataKeys;
+    if (pieces.length > 0 && isValidSubcategoryForCategory(pieces[0],
+                                                           r.category,
+                                                           mapping)) {
+        r.subcategory = pieces.shift();
+    }
+    if (pieces.length > 0) {
+        r.metadata = {};
+        _.each(pieces, function (v : string) {
+            var x = submap[v] || mapping.defaults[v];
+            if (x) {
+                r.metadata[x.key] = x.value;
+            }
+        });
+    }
+    return r;
+}
+
 
 /*
 idea:
@@ -21,54 +115,36 @@ use the deep-diff package
 make setlabel take two lists: one for nonterminals and one for terminals
   */
 
-enum LabelMapActionType {
-    Subcategory,
-    Metadata
-}
 
-interface LabelMapAction {
-    type : LabelMapActionType;
-    s : string;
-}
-
-interface LabelMap {
-    defaults : { [key: string] : LabelMapAction };
-    byPos : {
-        [label: string] : { [key: string] : LabelMapAction }
-    };
-}
 
 export function setLabelForNode (label : string,
                                  node : Element,
-                                 remove? : boolean) : void
+                                 remove? : boolean,
+                                 mapping : LabelMap = globals.labelMapping) : void
 {
-    pieces = label.split("-");
-    var category = pieces[0];
+    var pieces = label.split("-");
+    var category = pieces.shift();
     node.setAttribute("data-category", category);
-    var submapping = globals.labelMapping.byPos[category];
-    var didSubcategory = false;
-    _.map(pieces.slice(1), function (piece : string) : void {
-        var action = submapping && submapping[piece];
-        if (!action) action = globals.labelMapping.defaults[piece];
-        if (!action) return;
-        switch (action.type) {
-        case LabelMapActionType.Subcategory:
-            if (didSubcategory) {
-                throw "Found 2 subcategory specifications in mapping!";
-            }
-            if (remove) {
-                node.removeAttribute("data-subcategory");
-            } else {
-                node.setAttribute("data-subcategory", action.s);
-            }
-            didSubcategory = true;
-            break;
-        case LabelMapActionType.Metadata:
-            if (remove) {
-                utils.removeMetadata(node, s);
-            } else {
-                utils.setMetadata(node, s, "yes");
-            }
+    if (pieces.length > 1 && isValidSubcategoryForCategory(pieces[0],
+                                                           category,
+                                                           mapping)) {
+        if (remove) {
+            node.removeAttribute("data-subcategory");
+        } else {
+            node.setAttribute("data-subcategory", pieces[0]);
         }
-    })
+        pieces.shift();
+    }
+    if (pieces.length > 0) {
+        var submapping = mapping.byLabel[category] || {};
+        _.map(pieces, function (piece : string) : void {
+            var action = submapping[piece] || mapping.defaults[piece];
+            if (!action) return;
+            if (remove) {
+                utils.removeMetadata(node, action.key);
+            } else {
+                utils.setMetadata(node, action.key, action.value);
+            }
+        });
+    }
 }
