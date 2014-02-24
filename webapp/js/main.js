083d40d9ac346505881dadf4ed61a7cc26705329
Aaron Ecay <aaronecay@gmail.com>
2014-02-23 19:57:48 -0500

    main.js: insert default config if not already found

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,14 +1,29 @@
-/*global require: false */
+/*global require: false, __dirname: false */
 /*jshint browser: true */
 
 var vex = require("vex");
 vex.defaultOptions.className = "vex-theme-default";
 vex.dialog = require("vex-dialog");
 
-var ui = require("./ui/ui");
+var ui = require("./ui/ui"),
+    React = require("react"),
+    cs = require("./config-store"),
+    $ = require("jquery"),
+    fs = require("fs"),
+    _ = require("lodash");
 
-var React = require("react");
-
-document.addEventListener("DOMContentLoaded", function () {
-    React.renderComponent(ui.AnnotaldUI(), document.getElementById("mainui"));
+cs.listConfigs().then(function (configs) {
+    // TODO: this doesn't finish in time for the config to actually show up on
+    // the first run; the menu will be empty
+    if (! _.contains(_.keys(configs), "default")) {
+        return cs.setConfig("default", fs.readFileSync(
+            __dirname + "/../assets/settings.js"));
+    }
+    return undefined;
+}).then(function () {
+    $(function () {
+        React.renderComponent(ui.AnnotaldUI(), document.getElementById("mainui"));
+    });
+}).catch(function (e) {
+    console.log(e.stack);
 });
