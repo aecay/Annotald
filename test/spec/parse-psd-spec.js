f4cfe14f4307300f82831ce39cf776475e67a9f1
Aaron Ecay <aaronecay@gmail.com>
2014-04-30 22:51:52 -0400

    more tests and fixes for psd parser

--- test/spec/parse-psd-spec.js
+++ test/spec/parse-psd-spec.js
@@ -4,8 +4,7 @@
 
 var parsePsd = require("../../webapp/js/parse-psd.ts"),
     dummy = require("../../webapp/js/parse-psd"),
-    P = parsePsd.parseCorpus,
-    _ = require("lodash");
+    P = parsePsd.parseCorpus;
 
 function L(label, text, lemma, index) {
     var ret = {
@@ -17,9 +16,44 @@ function L(label, text, lemma, index) {
     return ret;
 }
 
+function T(label, index) {
+    var pieces = label.split("-");
+    var ret = {
+        category: pieces[0]
+    };
+    if (pieces.length > 1) ret.subcategory = ret.pieces.slice(1).join("-");
+    if (index) ret.index = index;
+    ret.desc = Array.prototype.slice.call(arguments, 2);
+    return ret;
+}
+
+function R(id, desc) {
+    var ret = { tree: desc };
+    if (id) ret.id = id;
+    return ret;
+}
+
 describe("The PSD parser", function () {
     it("should parse a leaf properly", function () {
         expect(P("( (FOO bar))")).toEqual(
             [{ tree: L("FOO", "bar") }]);
+    });
+    it("should parse a leaf with lemma properly", function () {
+        expect(P("( (FOO bar-baz))")).toEqual(
+            [{ tree: L("FOO", "bar", "baz") }]);
+    });
+    it("should parse a tree properly", function () {
+        expect(P("( (FOO (BAR baz)))")).toEqual(
+            [R(null,
+                T("FOO", null, L("BAR", "baz"))
+             )]
+        );
+    });
+    it("should parse a tree with ID properly", function () {
+        expect(P("( (FOO (BAR baz)) (ID xyz))")).toEqual(
+            [R("xyz",
+                T("FOO", null, L("BAR", "baz"))
+             )]
+        );
     });
 });
