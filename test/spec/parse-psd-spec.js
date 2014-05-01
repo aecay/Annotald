a0aae41da490a3dd1d2d049e5c193624b1177f9e
Aaron Ecay <aaronecay@gmail.com>
2014-04-30 21:50:47 -0400

    updates to parse psd test

--- test/spec/parse-psd-spec.js
+++ test/spec/parse-psd-spec.js
@@ -4,10 +4,23 @@
 
 var parsePsd = require("../../webapp/js/parse-psd.ts"),
     dummy = require("../../webapp/js/parse-psd"),
-    P = parsePsd.parseCorpus;
+    P = parsePsd.parseCorpus,
+    _ = require("lodash");
+
+function L(label, text, lemma, index) {
+    if (typeof lemma === "undefined") lemma = null;
+    return {
+        label: label,
+        text: text,
+        lemma: lemma,
+        index: index
+    };
+}
 
 describe("The PSD parser", function () {
     it("should parse a leaf properly", function () {
-        expect(P("( (FOO bar))")).toEqual({ label: "FOO", text: "bar" });
+        expect(P("( (FOO bar))")).toEqual(
+            [{ tree: L("FOO", "bar") }]);
+        expect(_.isEqual(P("( (FOO bar))"), [{ tree: L("FOO", "bar") }])).toBeTrue();
     });
 });
