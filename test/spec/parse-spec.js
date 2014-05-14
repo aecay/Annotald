b0c4de2080d058a2834b964251e17b5e9bb4820e
Aaron Ecay <aaronecay@gmail.com>
2014-05-14 19:11:29 -0400

    parse-spec.js: update

--- test/spec/parse-spec.js
+++ test/spec/parse-spec.js
@@ -10,12 +10,12 @@ describe("The parser", function () {
     var xml = '<corpus><sentence category="IP" subcategory="MAT"><nonterminal' +
             ' category="NP" subcategory="SBJ"><text'
             + ' category="PRO">I</text></nonterminal></sentence></corpus>'
-      , html = '<div class="snode" data-category="IP" data-subcategory="MAT"><div class="snode"'
+      , html = '<div class="snode" id="sn0"><div class="snode" data-category="IP" data-subcategory="MAT"><div class="snode"'
             + ' data-category="NP" data-subcategory="SBJ"><div class="snode"'
             + ' data-nodetype="text" data-category="PRO"><span '
-            + 'class="wnode">I</span></div></div></div>';
+            + 'class="wnode">I</span></div></div></div></div>';
     it("should generate correct HTML from XML", function () {
-        expect(parse.parseXmlToHtml(xml).innerHTML).toEqualString(html);
+        expect(parse.parseXmlToHtml(xml).outerHTML).toEqualString(html);
     });
     it("should generate correct XML from HTML", function () {
         expect(parse.parseHtmlToXml(html)).toEqualString(xml);
@@ -24,6 +24,6 @@ describe("The parser", function () {
         expect(parse.parseHtmlToXml(parse.parseXmlToHtml(xml))).toEqualString(xml);
     });
     it("should be idempotent on XML", function () {
-        expect(parse.parseXmlToHtml(parse.parseHtmlToXml(html)).innerHTML).toEqualString(html);
+        expect(parse.parseXmlToHtml(parse.parseHtmlToXml(html)).outerHTML).toEqualString(html);
     });
 });
