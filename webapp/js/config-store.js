0e1da1f95310fd9cf83775b64aeddc69ce45b6cf
Aaron Ecay <aaronecay@gmail.com>
2014-05-14 02:24:07 -0400

    config-store.js: convert to level

--- webapp/js/config-store.js
+++ webapp/js/config-store.js
@@ -1,31 +1,93 @@
 /*global require: false, exports: true */
 
-var localForage = require("localforage");
+var level = require("level-browserify");
+var Q = require("q");
+
+var db = level("annotald-config");
+
+function parse (x) {
+    try {
+        x = JSON.parse(x);
+    } catch (e) {
+        x = {};
+    }
+    return x;
+}
 
 exports.listConfigs = function listConfigs () {
-    return localForage.getItem("configs").then(function (x) {
-        return x || {};
+    var d = Q.defer();
+    db.get("configs", function (err, x) {
+        if (err) {
+            d.resolve({});
+        } else {
+            d.resolve(parse(x));
+        }
     });
+    return d.promise;
 };
 
 exports.setConfig = function setConfig (name, value) {
-    return localForage.getItem("configs").then(function (configs) {
-        configs = configs || {};
+    var d = Q.defer();
+    db.get("configs", function (err, configs) {
+        if (err) {
+            configs = "{}";
+        }
+        configs = parse(configs);
         configs[name] = value;
-        return localForage.setItem("configs", configs);
+        db.put("configs", JSON.stringify(configs), function (err) {
+            if (err) {
+                d.reject(err);
+            } else {
+                d.resolve();
+            }
+        });
     });
+    return d.promise;
 };
 
 exports.getConfig = function getConfig (name) {
-    return localForage.getItem("configs").then(function (configs) {
-        return configs && configs[name];
+    var d = Q.defer();
+    db.get("configs", function (err, configs) {
+        d.resolve(configs && parse(configs)[name]);
     });
+    return d.promise;
 };
 
 exports.deleteConfig = function deleteConfig (name) {
-    return localForage.getItem("configs").then(function (configs) {
-        configs = configs || {};
+    var d = Q.defer();
+    db.get("configs", function (err, configs) {
+        if (err || !configs) {
+            d.resolve();
+        }
+        configs = parse(configs);
         delete configs[name];
-        return localForage.setItem("configs", configs);
+        db.put("configs", JSON.stringify(configs), function (err) {
+            if (err) {
+                d.reject(err);
+            } else {
+                d.resolve();
+            }
+        });
     });
+    return d.promise;
+};
+
+exports.getLastConfig = function () {
+    var d = Q.defer();
+    db.get("lastConfig", function (err, lc) {
+        d.resolve(lc);
+    });
+    return d.promise;
+};
+
+exports.setLastConfig = function (val) {
+    var d = Q.defer();
+    db.put("lastConfig", val, function (err) {
+        if (err) {
+            d.reject(err);
+        } else {
+            d.resolve();
+        }
+    });
+    return d.promise;
 };
