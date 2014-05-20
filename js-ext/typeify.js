7c98ca5842283fadeff62fd73f6813188251bdc1
Aaron Ecay <aaronecay@gmail.com>
2014-05-20 13:35:31 -0400

    typeify transform: clean up, fix error reporting

--- js-ext/typeify.js
+++ js-ext/typeify.js
@@ -1,3 +1,4 @@
+/*global require: false, console: false, module: true */
 // type(script)ify v 0.1.0, author Andrew Gaspar, license MIT
 // Modified by Aaron Ecay
 
@@ -6,35 +7,32 @@ var fs = require("fs");
 var childproc = require("child_process");
 var path = require("path");
 
-var TypescriptCompileError = (function () {
-    function TypescriptCompileError() {
-        this.name = "Compile Error";
-        this.message = "Could not compile TypeScript file";
-    }
-    return TypescriptCompileError;
-})();
-
 function compile(file, data, cb) {
-    if (!isTypescript(file))
-        return cb(new Error("Not a typescript file"));
+    if (!isTypescript(file)) {
+        cb(new Error("Not a typescript file"));
+        return;
+    }
 
     var fileName = path.basename(file, ".ts");
     var dir = path.dirname(file);
     console.log("compiling: " + file);
 
-    childproc.exec("tsc -m commonjs " + file, { encoding: "utf8" }, function (error, stdout, stderr) {
-        if ((stdout && stdout.length > 0) || (stderr && stderr.length > 0)) {
-            console.log("Err: " + file);
-            var tce = new TypescriptCompileError();
-            tce.stdout = stdout.toString();
-            tce.stderr = stderr.toString();
-            return cb(tce);
-        } else {
-            fs.readFile(path.join(dir, fileName + ".js"), { encoding: "utf8" }, function (err, originalText) {
-                cb(err, originalText);
-                //convertRequirePaths(file, originalText, cb);
-            });
-        }
+    childproc.exec("tsc -m commonjs " + file,
+                   { encoding: "utf8" },
+                   function (error, stdout, stderr) {
+                       if ((stdout && stdout.length > 0) ||
+                           (stderr && stderr.length > 0)) {
+                           cb(new Error("Could not compile typescript file: " +
+                                        file + "\n" + stdout.toString() +
+                                        "\n" + stderr.toString()));
+                           return;
+                       } else {
+                           fs.readFile(path.join(dir, fileName + ".js"),
+                                       { encoding: "utf8" },
+                                       function (err, originalText) {
+                                           cb(err, originalText);
+                                       });
+                       }
     });
 }
 
@@ -49,10 +47,11 @@ function typeify(file) {
         file = candidateReplacement;
         console.log("replaced with " + file);
     }
-    if (!isTypescript(file))
+    if (!isTypescript(file)){
         return through();
+    }
 
-    var data = '';
+    var data = "";
     return through(write, end);
 
     function write(buf) {
@@ -61,14 +60,13 @@ function typeify(file) {
     function end() {
         var _this = this;
         compile(file, data, function (err, data) {
-            if (err)
-                _this.emit('error', err);
-
+            if (err) {
+                _this.emit("error", err);
+            }
             _this.queue(data);
             _this.queue(null);
         });
     }
 }
-;
 
 module.exports = typeify;
