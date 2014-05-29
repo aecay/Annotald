002f2deb03fbb66ec5607b0b68d0f87690ba657e
Aaron Ecay <aaronecay@gmail.com>
2014-05-29 02:27:00 -0400

    test/spec/metadata-spec.js: reorganize

--- test/spec/metadata-spec.js
+++ test/spec/metadata-spec.js
@@ -9,32 +9,34 @@ describe("The metadata module", function () {
     it("should export its test functions properly", function () {
         expect(setInDict instanceof Function).toBeTruthy();
     });
-    it("should set in dict properly", function () {
-        expect(setInDict({}, "foo", "bar")).toDeepEqual({foo: "bar"});
-    });
-    it("should set in dict properly (deep)", function () {
-        expect(setInDict({}, "foo", {bar : "baz"})).toDeepEqual({foo: {bar : "baz"}});
-    });
-    it("should set in dict properly (deep, multiple keys)", function () {
-        expect(setInDict({}, "foo", {bar: "baz", quux: "abc"})).toDeepEqual(
-            {foo: {bar : "baz", quux: "abc"}});
-    });
-    it("should set in dict properly ignoring other keys", function () {
+    describe("setInDict", function () {
+        it("should set in dict properly", function () {
+            expect(setInDict({}, "foo", "bar")).toDeepEqual({foo: "bar"});
+        });
+        it("should set in dict properly (deep)", function () {
+            expect(setInDict({}, "foo", {bar : "baz"})).toDeepEqual({foo: {bar : "baz"}});
+        });
+        it("should set in dict properly (deep, multiple keys)", function () {
+            expect(setInDict({}, "foo", {bar: "baz", quux: "abc"})).toDeepEqual(
+                {foo: {bar : "baz", quux: "abc"}});
+        });
+        it("should set in dict properly ignoring other keys", function () {
             expect(setInDict({a: "b"}, "foo", "bar")).toDeepEqual({a: "b", foo: "bar"});
-    });
-    it("should set in dict properly (deep) ignoring other keys", function () {
-        expect(setInDict({a: "b"}, "foo", {bar : "baz"})).toDeepEqual({a: "b",
-                                                                       foo: {bar : "baz"}});
-    });
-    it("should set in dict properly (deep) ignoring other keys (deep)", function () {
-        expect(setInDict({foo: {quux: "abc"}}, "foo", {bar : "baz"})).toDeepEqual(
-            {foo: {quux: "abc", bar : "baz"}});
-    });
-    it("should replace in dict properly", function () {
-        expect(setInDict({foo: "quux"}, "foo", "bar")).toDeepEqual({foo: "bar"});
-    });
-    it("should replace in dict properly (deep)", function () {
-        expect(setInDict({foo: "quux"}, "foo", {bar : "baz"})).toDeepEqual(
-            {foo: {bar : "baz"}});
+        });
+        it("should set in dict properly (deep) ignoring other keys", function () {
+            expect(setInDict({a: "b"}, "foo", {bar : "baz"})).toDeepEqual({a: "b",
+                                                                           foo: {bar : "baz"}});
+        });
+        it("should set in dict properly (deep) ignoring other keys (deep)", function () {
+            expect(setInDict({foo: {quux: "abc"}}, "foo", {bar : "baz"})).toDeepEqual(
+                {foo: {quux: "abc", bar : "baz"}});
+        });
+        it("should replace in dict properly", function () {
+            expect(setInDict({foo: "quux"}, "foo", "bar")).toDeepEqual({foo: "bar"});
+        });
+        it("should replace in dict properly (deep)", function () {
+            expect(setInDict({foo: "quux"}, "foo", {bar : "baz"})).toDeepEqual(
+                {foo: {bar : "baz"}});
+        });
     });
 });
