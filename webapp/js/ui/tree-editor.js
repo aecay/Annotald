78dbfe547729af5a3259abbbf4776c56dd529511
Aaron Ecay <aaronecay@gmail.com>
2014-05-30 02:44:56 -0400

    ui/tree-editor.js: support loading psd files

--- webapp/js/ui/tree-editor.js
+++ webapp/js/ui/tree-editor.js
@@ -3,12 +3,15 @@
 /*jshint browser: true */
 
 var React = require("react"),
-    parser = require("../parse"),
+    parser = require("../parse.ts"),
+    psdParser = require("../parse-psd.ts"),
     template = require("./tree-editor-template").template,
     $ = require("jquery"),
     configStore = require("../config-store"),
     treedrawing = require("../treedrawing/startup.ts"),
-    Q = require("q");
+    Q = require("q"),
+    globals = require("../treedrawing/global"),
+    db = require("../db");
 
 exports.TreeEditor = React.createClass({
     exit: function () {
@@ -26,6 +29,18 @@ exports.TreeEditor = React.createClass({
                 script.appendTo("head");
             }),
             this.props.file.read().then(function (content) {
+                if ($.trim(content).substr(0, 1) === "(") {
+                    // old-format PSD file
+                    return db.get("formats").then(function (fmts) {
+                        return fmts[that.props.fmtName];
+                    }).then(function (format) {
+                        return psdParser.jsToXml(psdParser.parseCorpus(content),
+                                                 parser.parseFormatSpec(format));
+                    });
+                } else {
+                    return content;
+                }
+            }).then(function (content) {
                 var html = parser.parseXmlToHtml(content);
                 $("#editpane").html(html);
             })]).done(function () {
