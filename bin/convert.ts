ba881aa7294f56e9b0f9743a783a2f9d5e1d4b04
Aaron Ecay <aaronecay@gmail.com>
2014-06-30 05:25:18 -0400

    bin/convert.ts: update

--- bin/convert.ts
+++ bin/convert.ts
@@ -1,8 +1,9 @@
 ///<reference path="./../types/all.d.ts" />
 
 require("typescript-require");
-import parsePsd = require("./../webapp/js/parse-psd");
-import parse = require("./../webapp/js/parse");
+import parsePsd = require("./../src/js/parse-psd");
+import parse = require("./../src/js/parse");
+import lc = require("./../src/js/treedrawing/label-convert");
 import fs = require("fs");
 
 var xmldom = require("xmldom");
@@ -14,10 +15,10 @@ var DOMParser = new xmldom.DOMParser();
 if (args.xml) {
     var f = fs.readFileSync(args._[0]).toString();
     var iceFormat = <Element>DOMParser.parseFromString(fs.readFileSync(
-        __dirname + "/../webapp/js/psd-grammars/icelandic-format.xml").toString())
+        __dirname + "/../src/js/psd-grammars/icelandic-format.xml").toString())
         .childNodes[0];
     process.stdout.write(
         parsePsd.jsToXml(parsePsd.parseCorpus(f),
-                         parse.parseFormatSpec(iceFormat),
+                         lc.parseFormatSpec(iceFormat),
                          parsePsd.corpusDefs["icepahc"]));
 }
